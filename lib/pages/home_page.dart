1576b1c0c396e0ecbbc6e2321df62e45a460f42b
jhdodev <jhdo.dev@gmail.com>
2024-10-16 20:10:29 +0900

    [feature] 등락률 상위 종목 및 거래대금 상위 종목 표시 기능 추가

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'dart:convert'; // JSON 파싱을 위한 import
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:intl/intl.dart'; // NumberFormat을 사용하기 위한 import
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,30 +12,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<dynamic> stockData = [];
+  List<dynamic> stockDataFltRt = [];
+  List<dynamic> stockDataTrPrc = [];
   bool isLoading = true; // 데이터를 로드 중인지 여부
 
-  // API 호출 함수
-  Future<void> fetchStockData() async {
+  // API 호출 함수 (등락률 20% 이상 종목 조회)
+  Future<void> fetchStockDataFltRt() async {
     final String apiKey = dotenv.env['API_KEY'] ?? ''; // .env 파일에서 API_KEY를 가져옴
 
     const String baseUrl =
         'https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo';
     final String apiUrl =
-        '$baseUrl?serviceKey=$apiKey&numOfRows=10&resultType=json';
+        '$baseUrl?serviceKey=$apiKey&numOfRows=10&beginFltRt=20&resultType=json';
 
     try {
       final response = await http.get(Uri.parse(apiUrl));
 
       if (response.statusCode == 200) {
-        // API 응답을 JSON으로 변환
         final parsedData = json.decode(response.body);
 
-        // 필요한 데이터 추출
         setState(() {
-          stockData = parsedData['response']['body']['items']
-              ['item']; // 실제 데이터 경로는 API 구조에 따라 다를 수 있음
-          isLoading = false; // 데이터 로딩 완료
+          stockDataFltRt = (parsedData['response']['body']['items']['item']
+                  as List<dynamic>)
+              .where((item) => item['fltRt'] != null)
+              .toList()
+            ..sort((a, b) => double.parse(b['fltRt'].toString())
+                .compareTo(double.parse(a['fltRt'].toString())));
+
+          isLoading = false;
         });
       } else {
         throw Exception('Failed to load stock data');
@@ -42,7 +47,43 @@ class _HomePageState extends State<HomePage> {
     } catch (e) {
       print('Error: $e');
       setState(() {
-        isLoading = false; // 오류 발생 시 로딩 중지
+        isLoading = false;
+      });
+    }
+  }
+
+  // API 호출 함수 (거래대금 상위 종목 조회)
+  Future<void> fetchStockDataTrPrc() async {
+    final String apiKey = dotenv.env['API_KEY'] ?? '';
+
+    const String baseUrl =
+        'https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo';
+    final String apiUrl =
+        '$baseUrl?serviceKey=$apiKey&numOfRows=10&resultType=json&beginTrPrc=1000000000'; // 거래대금 10억 이상
+
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        final parsedData = json.decode(response.body);
+
+        setState(() {
+          stockDataTrPrc = (parsedData['response']['body']['items']['item']
+                  as List<dynamic>)
+              .where((item) => item['trPrc'] != null)
+              .toList()
+            ..sort((a, b) => double.parse(b['trPrc'].toString())
+                .compareTo(double.parse(a['trPrc'].toString())));
+
+          isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to load stock data');
+      }
+    } catch (e) {
+      print('Error: $e');
+      setState(() {
+        isLoading = false;
       });
     }
   }
@@ -50,75 +91,131 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    fetchStockData(); // 페이지가 로드될 때 API 호출
+    fetchStockDataFltRt(); // 등락률 상위 데이터 호출
+    fetchStockDataTrPrc(); // 거래대금 상위 데이터 호출
+  }
+
+  // 숫자 형식 변환 함수
+  String formatNumber(String number) {
+    final int? parsedNumber = int.tryParse(number);
+    if (parsedNumber != null) {
+      return NumberFormat('###,###,###,###').format(parsedNumber);
+    }
+    return number;
+  }
+
+  // 수정된 buildStockCard 함수
+  Widget buildStockCard(Map<String, dynamic> stock, int index) {
+    // 주식 정보
+    final String stockName = stock['itmsNm']?.toString() ?? 'Unknown Stock';
+    final String price = formatNumber(stock['clpr']?.toString() ?? '0');
+    final String changePrice = formatNumber(stock['vs']?.toString() ?? '0');
+    final String changeRate = stock['fltRt']?.toString() ?? '0';
+    final String trPrc = formatNumber(stock['trPrc']?.toString() ?? '0');
+
+    // 상승/하락 판단 및 색상/아이콘 설정
+    final double? parsedChangePrice =
+        double.tryParse(changePrice.replaceAll(',', '')); // 쉼표 제거 후 숫자 변환
+    final bool isRising = parsedChangePrice != null && parsedChangePrice > 0;
+    final Color changeColor = isRising ? Colors.red : Colors.blue;
+    final String icon = isRising ? '▲' : '▼';
+
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '${index + 1}. $stockName',
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 4),
+            Text('가격: $price원', style: const TextStyle(fontSize: 14)),
+            Row(
+              children: [
+                Text(
+                  icon,
+                  style: TextStyle(color: changeColor, fontSize: 14),
+                ),
+                Text(
+                  '$changePrice $changeRate%',
+                  style: TextStyle(color: changeColor, fontSize: 14),
+                ),
+              ],
+            ),
+            Text('거래대금: $trPrc원', style: const TextStyle(fontSize: 14)),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('홈'),
+        title: const Text('종목 데이터'),
       ),
       body: isLoading
-          ? const Center(child: CircularProgressIndicator()) // 데이터가 로딩 중일 때 표시
-          : stockData.isEmpty
-              ? const Center(child: Text('데이터를 불러올 수 없습니다.'))
-              : ListView.builder(
-                  itemCount: stockData.length,
-                  itemBuilder: (context, index) {
-                    final stock = stockData[index];
-
-                    // 주식 정보
-                    final String stockName =
-                        stock['itmsNm'] ?? 'Unknown Stock'; // 종목명
-                    final String price = stock['clpr'] ?? '0'; // 현재 가격
-                    final String changePrice = stock['vs'] ?? '0'; // 등락폭
-                    final String changeRate = stock['fltRt'] ?? '0'; // 등락률
-
-                    // 상승/하락률이 양수인지 음수인지에 따라 텍스트 색상 변경
-                    final double? parsedChangePrice =
-                        double.tryParse(changePrice);
-                    final Color changeColor =
-                        (parsedChangePrice != null && parsedChangePrice > 0)
-                            ? Colors.red // 상승: 빨간색
-                            : Colors.blue; // 하락: 파란색
-
-                    return Card(
-                      margin: const EdgeInsets.all(8.0),
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              stockName,
-                              style: const TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold),
-                            ), // 예: 주식 이름
-                            const SizedBox(height: 8),
-                            Text(
-                              '$price원', // 예: 주식 가격
-                              style: const TextStyle(fontSize: 16),
-                            ),
-                            const SizedBox(height: 8),
-                            Row(
-                              children: [
-                                Text(
-                                  '($changePrice, ',
-                                  style: TextStyle(color: changeColor),
-                                ),
-                                Text(
-                                  '$changeRate%)',
-                                  style: TextStyle(color: changeColor),
-                                ),
-                              ],
-                            ),
-                          ],
+          ? const Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              child: Column(
+                children: [
+                  // 등락률 상위 종목 섹션
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          '등락률 상위 종목',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
                         ),
-                      ),
-                    );
-                  },
-                ),
+                        const SizedBox(height: 10),
+                        ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: stockDataFltRt.length,
+                          itemBuilder: (context, index) {
+                            final stock = stockDataFltRt[index];
+                            return buildStockCard(stock, index);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+
+                  // 거래대금 상위 종목 섹션
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          '거래대금 상위 종목',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 10),
+                        ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: stockDataTrPrc.length,
+                          itemBuilder: (context, index) {
+                            final stock = stockDataTrPrc[index];
+                            return buildStockCard(stock, index);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
